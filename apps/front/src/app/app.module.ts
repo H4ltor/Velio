eff8b5f412f63906c64873c1ce08b10ac64d8784
valentin arpin <valentin.arpin@ynov.com>
2021-12-01 17:32:47 +0100

    [Add] Création routing

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { VelioRoutingModule } from './velio-routing/velio-routing.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './Components/home/home.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NavComponent, FooterComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule,
     NoopAnimationsModule,
+    VelioRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
