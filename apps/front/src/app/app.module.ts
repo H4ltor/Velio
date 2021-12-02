42dee03e6d2f6e862817e21ddc1501e6fba51fed
valentin arpin <valentin.arpin@ynov.com>
2021-12-02 11:26:31 +0100

    [Add] Création Services & Class, [Add] Création Componentss & Architecture projet

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -8,10 +8,19 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './Components/home/home.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { ListBikesComponent } from './Manager/list-bikes/list-bikes.component';
+import { ListParksComponent } from './Manager/list-parks/list-parks.component';
+import { CreateParkComponent } from './Admin/Create/create-park/create-park.component';
+import { ShowParkComponent } from './Admin/Show/show-park/show-park.component';
+import { ShowBikeComponent } from './Admin/Show/show-bike/show-bike.component';
+import { CreateBikeComponent } from './Admin/Create/create-bike/create-bike.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent,
+    NavComponent, FooterComponent,
+    ListBikesComponent, ListParksComponent, CreateParkComponent, ShowParkComponent, ShowBikeComponent, CreateBikeComponent,
+    ],
   imports: [
     BrowserModule,
     RouterModule,
