89e82271b8505a6b8a8ef4eb5896575f5ad07b43
Timothée Cognard <timothee.cognard@ynov.com>
2021-12-02 15:35:04 +0100

    [Add] Ajout des cards + 1er mise en forme home

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { VelioRoutingModule } from './velio-routing.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './Components/home/home.component';
-import { NavComponent } from './Components/nav/nav.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { ListBikesComponent } from './Manager/list-bikes/list-bikes.component';
-import { ListParksComponent } from './Manager/list-parks/list-parks.component';
-import { CreateParkComponent } from './Admin/Create/create-park/create-park.component';
-import { ShowParkComponent } from './Admin/Show/show-park/show-park.component';
-import { ShowBikeComponent } from './Admin/Show/show-bike/show-bike.component';
-import { CreateBikeComponent } from './Admin/Create/create-bike/create-bike.component';
+import { HomeComponent } from './Components/public/home/home.component';
+import { NavComponent } from './Components/public/nav/nav.component';
+import { FooterComponent } from './Components/public/footer/footer.component';
+import { ListBikesComponent } from './Components/Manager/list-bikes/list-bikes.component';
+import { ListParksComponent } from './Components/Manager/list-parks/list-parks.component';
+import { CreateParkComponent } from './Components/Admin/Create/create-park/create-park.component';
+import { ShowParkComponent } from './Components/Admin/Show/show-park/show-park.component';
+import { ShowBikeComponent } from './Components/Admin/Show/show-bike/show-bike.component';
+import { CreateBikeComponent } from './Components/Admin/Create/create-bike/create-bike.component';
+import { MaterialModule } from './_shared/material/material.module';
 
 
 @NgModule({
@@ -22,12 +23,13 @@ import { CreateBikeComponent } from './Admin/Create/create-bike/create-bike.comp
     ListBikesComponent, ListParksComponent,
     CreateParkComponent, ShowParkComponent,
     ShowBikeComponent, CreateBikeComponent,
-    ],
+  ],
   imports: [
     BrowserModule,
     RouterModule,
     NoopAnimationsModule,
-    VelioRoutingModule
+    VelioRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/front/src/app/velio-routing.module.ts
+++ apps/front/src/app/velio-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent} from './Components/home/home.component';
-import { ListParksComponent } from './Manager/list-parks/list-parks.component';
-import { ListBikesComponent } from './Manager/list-bikes/list-bikes.component';
+import { HomeComponent} from './Components/public/home/home.component';
+import { ListParksComponent } from './Components/Manager/list-parks/list-parks.component';
+import { ListBikesComponent } from './Components/Manager/list-bikes/list-bikes.component';
+import { CreateBikeComponent } from './Components/Admin/Create/create-bike/create-bike.component';
+import { CreateParkComponent } from './Components/Admin/Create/create-park/create-park.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'create-bikes', component: CreateBikeComponent},
+  {path: 'create-parks', component: CreateParkComponent},
   {path: 'list-parks', component: ListParksComponent},
   {path: 'list-bikes', component: ListBikesComponent},
-]
+];
 @NgModule({
   declarations: [],
   imports: [
